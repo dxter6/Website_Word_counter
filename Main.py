06a1fa39d20b018a8c70321114bfe88ebc621e8b
Jesus <jesus@pop-os.localdomain>
2020-03-25 16:34:07 +0530

    Login and Signup Working

--- Main.py
+++ Main.py
@@ -5,16 +5,38 @@ from flask import session
 from flask import redirect
 from flask import g
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField
 from wtforms.validators import InputRequired,Email,Length
+from wtforms import ValidationError
 import os
 import requests
+
+#-------------->Os-database-environment<--------#
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 #-------->flask-intilization<-------------------#
 app = Flask(__name__,template_folder='templates')
 Bootstrap(app)
+#app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/jesus/proj/edyst-challenge/Main/database.db'
+db = SQLAlchemy(app)
+
+# User calss
+class Users(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(50),unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
 
 #Login form creation in FlaskForm
 class LoginForm(FlaskForm):
@@ -35,9 +57,16 @@ def index():
     return render_template('index.html')
 
 #Login
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            if user.password == form.password.data:
+                return redirect(url_for('dashboard'))
+        return "<h1>Invalid Credientials</h1>"
+        # return form.username.data+" "+form.password.data
 
     return render_template('login.html',form=form)
 
@@ -45,7 +74,12 @@ def login():
 @app.route('/signup',methods=['GET','POST'])
 def signup():
     form = RegisterForm()
-
+    if form.validate_on_submit():
+       new_user = Users(username = form.username.data,email=form.email.data,password=form.password.data)
+       db.session.add(new_user)
+       db.session.commit()
+       return '<h1>New user Created</h1>'
+       # return form.username.data+" "+form.password.data+" "+form.email.data
     return render_template('signup.html',form=form)
 
 #dashboard
