33728da38fad43e74c05a31342fda5a21af4b6a6
Jesus <jesus@pop-os.localdomain>
2020-03-25 17:08:59 +0530

    Minor fix with user authorization and session

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from flask import session
 from flask import redirect
 from flask import g
 from flask import url_for
+from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user,current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -30,14 +31,21 @@ Bootstrap(app)
 #app.config.from_object(Config)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/jesus/proj/edyst-challenge/Main/database.db'
 db = SQLAlchemy(app)
+# Flask_login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 # User calss
-class Users(db.Model):
+class Users(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(50),unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 #Login form creation in FlaskForm
 class LoginForm(FlaskForm):
@@ -65,6 +73,7 @@ def login():
         user = Users.query.filter_by(username=form.username.data).first()
         if user:
             if check_password_hash(user.password,form.password.data):
+                login_user(user,remember=form.remember.data)
                 return redirect(url_for('dashboard'))
         return "<h1>Invalid Credientials</h1>"
         # return form.username.data+" "+form.password.data
@@ -86,6 +95,7 @@ def signup():
 
 #dashboard
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
