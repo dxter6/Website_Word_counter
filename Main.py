4cd49c9d705317bb3b259090990c73156e6202cd
Jesus <jesus@pop-os.localdomain>
2020-03-25 13:19:15 +0530

    Total redesign

--- Main.py
+++ Main.py
@@ -1,46 +1,36 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
+from flask import redirect
+from flask import g
+from flask import url_for
+import os
 import requests
 
+#-------->flask-intilization<-------------------#
 app = Flask(__name__,template_folder='templates')
 
-#---------->Index page defining<-----------------#
-@app.route('/',methods=['GET'])
+#--------->Taking-unique-Key<-------------------#
+app.secret_key = os.urandom(24)
+
+#Index
+@app.route('/')
 def index():
     return render_template('index.html')
-#---------->user-Page-with-name<----------------#
-@app.route('/user/<string:name>',methods=['GET'])
-def user(name):
-    if name:
-        return render_template('user.html',name=name)
-#---------->user-page-for-username-------------#
-@app.route('/user/')
-def users():
-    return render_template('user.html')
- 
-#--------->submits-Page<-----------------------#
-@app.route('/submits',methods=['GET'])
-def submits():
-    return render_template('submits.html')
-
-#--------->Login-Page<-------------------------#
-@app.route('/login',methods=['GET','POST'])
+#Login
+@app.route('/login')
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] == 'abhinav' or request.form['password'] == 'abhinav@123':
-            return render_template('user.html',name='abhinav')
-        elif request.form['username'] == 'aneeq' or request.form['password'] == 'aneeq@123':
-            return render_template('user.html',name='aneeq')
-        elif  request.form['username'] == 'me' or request.form['password'] == 'me@123':
-            return render_template('user.html',name='Me')
-        else:
-            error = "Invalid credientials Please try again"
-    return render_template('login.html',error=error)
+    return render_template('login.html')
+#Signup
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    return render_template('signup.html')
+#dashboard
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
-
-#--------->for-instat-app-running-&-debug<------#
 if __name__ == "__main__":
     app.run(debug=True)
